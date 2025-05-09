a09ad45091416576d4d47914454721ca8c629117
viper-n <github@mailsecure.mozmail.com>
2025-05-09 13:29:59 -0400

    L9: break down wall

--- cell.py
+++ cell.py
@@ -2,7 +2,7 @@ from graphics import Line, Point
 
 
 class Cell:
-    def __init__(self, win = None):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -15,7 +15,7 @@ class Cell:
 
     def draw(self, x1, y1, x2, y2):
         if self._win is None:
-            return        
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -23,16 +23,28 @@ class Cell:
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
-    
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
+
     def draw_move(self, to_cell, undo=False):
         half_length = abs(self._x2 - self._x1) // 2
         x_center = half_length + self._x1
--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 from cell import Cell
+import random
 import time
 
 
@@ -11,7 +12,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win = None,
+        win=None,
     ):
         self._cells = []
         self._x1 = x1
@@ -23,6 +24,7 @@ class Maze:
         self._win = win
 
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -49,3 +51,9 @@ class Maze:
             return
         self._win.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
