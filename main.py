ca0b447355ba2af0afb845a96e8425b3102aa38b
viper-n <github@mailsecure.mozmail.com>
2025-05-08 20:58:02 -0400

    L5: Cell

--- main.py
+++ main.py
@@ -1,66 +1,27 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window
+from cell import Cell
 
-class Window():
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-        self.__running = False
-
-        self.__root = Tk()
-        self.__root.title("Maze Solver")
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        
-        self.__canvas = Canvas(self.__root)
-        self.__canvas.pack(fill = BOTH, expand = True)
-    
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-    
-    def wait_for_close(self):
-        self.__running = True
-        while self.__running:
-            self.redraw()
-    
-    def close(self):
-        self.__running = False
-    
-    def draw_line(self, line, fill_color):
-        line.draw(self.__canvas, fill_color)
-
-class Point():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-class Line():
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
-    
-    def draw(self, canvas, fill_color):
-        canvas.create_line(
-            self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
-        )
-        
 
 def main():
     win = Window(800, 600)
 
-    # Create some points
-    p1 = Point(100, 100)
-    p2 = Point(200, 200)
-    p3 = Point(100, 200)
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
 
-    # Create lines using those points
-    line1 = Line(p1, p2)
-    line2 = Line(p2, p3)
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
 
-    # Draw the lines with different colors
-    win.draw_line(line1, "red")
-    win.draw_line(line2, "blue")
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
 
     win.wait_for_close()
+
 
 main()
